4226d4a9293ad6075e46e4162bfa63e9ac67956f
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-03 11:01:23 -0700

    added private

--- instance_mysql.tf
+++ instance_mysql.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "web1" {
+resource "aws_instance" "mysql" {
 
     ami= "${var.ami}"
     instance_type = "${var.instance_type}"
     key_name = "${var.key_name}"
-    subnet_id = "${aws_subnet.public.id}"
+    subnet_id = "${aws_subnet.private.id}"
     user_data = "${file("userdata.sh")}"
-    vpc_security_group_ids  = ["${aws_security_group.public.id}"]
+    vpc_security_group_ids  = ["${aws_security_group.privateDB.id}"]
     associate_public_ip_address = "true"
     tags = {
-      Name = "${var.Name}"
+      Name = "${var.Name}.MYSQL"
       Env = "${var.Env}"
       Created_by = "${var.Created_by}"
       Dept = "${var.Dept}"

9a15184cd97de460aae021a9191ecfe2f2de4256
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-03 10:04:53 -0700

    added

--- public.sg.tf
+++ public.sg.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "public" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "[0.0.0.0/0]"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {

1ab35486ceac7d00d3140606983a577a1a6d2427
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-03 09:33:52 -0700

    added private

--- subnet.tf
+++ subnet.tf
@@ -9,3 +9,15 @@ resource "aws_subnet" "public" {
       Dept = "${var.Dept}"
   }
 }
+
+resource "aws_subnet" "private" {
+  vpc_id     = "${aws_vpc.dev.id}"
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+      Name = "${var.Name}.private"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
+  }
+}

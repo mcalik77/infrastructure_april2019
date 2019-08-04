8397bf79de73f3dc7e3e9f0ab05c7719c09ce06c
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-04 12:18:09 -0700

    save

---
+++ bucket.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "bucketname" {
+    bucket = "infrastructure-april-mustafa-2019a"
+}
---
+++ dont_create.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "bucketnameb" {
+    bucket = "infrastructure-april-mustafa-2019b"
+}
